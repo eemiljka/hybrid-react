ca5dba46b12afab35cc1018b30af05c52df2c603
eemiljka <eemiljka@metropolia.fi>
2024-03-12 17:53:36 +0200

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        'platinum': '#E0E1DD',
-        'silverblue': '#778DA9',
-        'blue': '#415A77',
-        'darkblue': '#1B263B',
-        'black': '#0D1B2A',
+        platinum: '#E0E1DD',
+        silverblue: '#778DA9',
+        blue: '#415A77',
+        darkblue: '#1B263B',
+        black: '#0D1B2A',
+      },
+      backgroundImage: () => ({
+        'custom-bg':
+          'url(https://images.pexels.com/photos/1154504/pexels-photo-1154504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)',
+      }),
+      backgroundSize: {
+        'size-contain': 'contain',
+      },
+      backgroundAttachment: {
+        fixed: 'fixed',
       },
     },
   },
